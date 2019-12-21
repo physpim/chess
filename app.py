f49d77a9bcda76b9b67a986487ef95c45fc3db3a
Pim Venderbosch <venderbosch.pim@hotmail.com>
2019-12-21 16:56:57 +0100

    clean up

--- app.py
+++ app.py
@@ -6,13 +6,3 @@ ui = Ui()
 # Make a turn
 for n in range(4):
     ui.turn()
-
-# Things to implement:
-# !!! Check detection
-# !!! Save game
-# !!! End game
-# !!! GUI
-# !!! Castling
-# !!! Check if under attack
-# !!! Check what a piece attacks
-# !!! Promoting a pawn
